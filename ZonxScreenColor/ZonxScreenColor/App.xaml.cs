f113262f61aaf1267a798493edcabc4d0fa11608
lingme <kid--l@hotmail.com>
2018-06-04 17:19:19 +0800

    add mutex open

--- ZonxScreenColor/ZonxScreenColor/App.xaml.cs
+++ ZonxScreenColor/ZonxScreenColor/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,17 @@ namespace ZonxScreenColor
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex mutex = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var isRuning = false;
+            mutex = new Mutex(true, "ZonxScreenColor", out isRuning);
+            if (!isRuning)
+            {
+                MessageBox.Show("程序已在运行，请勿重复启动！", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Environment.Exit(0);
+            }
+            base.OnStartup(e);
+        }
     }
 }
